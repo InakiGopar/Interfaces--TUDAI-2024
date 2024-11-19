b921d1eee59d07c7cf0e4f8f3a76423aa3176c0f
fermin villar gonzalez <benya_vg@hotmail.com>
2024-11-19 16:17:32 -0300

    Se avanzaron secciones 8 ,9, 10 y 11

--- TP4/js/funnyApp.js
+++ TP4/js/funnyApp.js
@@ -27,17 +27,27 @@ setInterval(changeImg, 3000);
 //Cards
 document.addEventListener("DOMContentLoaded", () => {
     const cards = document.querySelectorAll(".card");
+
+    //IntersectionObserver permite detectar cuándo un elemento del DOM entra o sale del área visible del navegador.
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry, index) => {
+            //si la card está visible
             if (entry.isIntersecting) {
+                // Agregar la clase "visible" con un retraso
                 setTimeout(() => {
                     entry.target.classList.add("visible");
-                }, index * 300); // Retraso de 0.3s por card
+                }, index * 300); // Retraso de 0.3s por tarjeta
+            } 
+            else {
+                // eliminar la clase "visible" cuando ya no esté visible
+                entry.target.classList.remove("visible");
             }
         });
-    }, {
+    }, 
+    {
         threshold: 0.5 // Se activa cuando el 50% de la card está visible
     });
 
+    // Observa cada tarjeta
     cards.forEach((card) => observer.observe(card));
 });
--- TP4/js/show-video.js
+++ TP4/js/show-video.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const section = document.querySelector('#video-section');
-    const videoContent = document.querySelector('.video-content');
-    const characterContent = document.querySelector('.character-content');
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          // Aplica la animación cuando la sección está visible
-          videoContent.style.transform = 'translateY(0)';
-          characterContent.style.transform = 'translateY(50px)';
-        }
-      });
-    }, { threshold: 0.1 }); // Detecta cuando el 10% de la sección es visible
-  
-    observer.observe(section);
-  });
+const moves = document.querySelectorAll(".move");
+
+// Efecto parallax sutil
+let lastScrollPosition = 0;
+
+window.addEventListener("scroll", () => {
+    lastScrollPosition = window.scrollY;
+    requestAnimationFrame(applyParallaxEffect);
+});
+
+function applyParallaxEffect() {
+    moves.forEach((move, index) => {
+        const depth = (index + 1) * 0.0010; // Profundidad ajustada para un efecto más sutil
+        const translateY = lastScrollPosition * depth;
+        move.style.transform = `translateY(${translateY}px)`;
+    });
+}
